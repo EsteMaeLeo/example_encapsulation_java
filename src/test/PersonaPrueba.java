860303893373740aa82d811cd74c1f8c39177851
EsteMaeLeo <leonel_morales20@hotmail.com>
2022-06-10 14:15:49 -0500

    This example java code

---
+++ src/test/PersonaPrueba.java
@@ -0,0 +1,17 @@
+package test;
+
+import dominio.*;
+
+public class PersonaPrueba {
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan", 5000.00, false);
+        //System.out.println("persona nombre: " + persona1.getNombre());
+        System.out.println(persona1);
+        persona1.setNombre("Juan Carlos");
+        //System.out.println("persona nombre: " + persona1.getNombre());
+        //System.out.println("persona1 sueldo = " + persona1.getSueldo());
+        //System.out.println("persona1 eliminado = " + persona1.isEliminado());
+        System.out.println(persona1.toString());
+    }
+}
